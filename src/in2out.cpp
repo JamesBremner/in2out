d6e0e842e074aedeaf849ecf3533b7eb95581c1c
JamesBremner <james@ravenspoint.com>
2022-08-10 11:18:49 -0400

    Input server

--- src/in2out.cpp
+++ src/in2out.cpp
@@ -6,21 +6,75 @@
 #include <algorithm>
 #include <boost/program_options.hpp>
 #include <wex.h>
+#include "tcp.h"
+#include "cStarterGUI.h"
 
 class cIn2Out
 {
 public:
     cIn2Out();
     void ParseOptions(int ac, char **av);
+    int inputPort() const
+    {
+        return myInputPort;
+    }
+    std::string sInputPort() const
+    {
+        return std::to_string(myInputPort);
+    }
 
 private:
     int myInputPort;
     std::string myOutputIP;
-     int myOutputPort;
+    int myOutputPort;
+};
+class cGUI : public cStarterGUI
+{
+public:
+    cGUI(int ac, char **av)
+        : cStarterGUI(
+              "Starter",
+              {50, 50, 1000, 500}),
+          myTCPinput(wex::maker::make<wex::tcp>(fm))
+    {
+        in2out.ParseOptions(ac, av);
+
+        myStartTimer = new wex::timer(fm, 1000);
+        fm.events().timer(
+            [this](int id)
+            {
+                delete myStartTimer;
+                connect();
+            });
+
+        fm.events()
+            .tcpServerAccept(
+                [this]
+                {
+                    std::cout << "Input source connected\n";
+                    myTCPinput.read();
+                });
+
+        fm.events().tcpRead(
+            [this]
+            {
+                input();
+            });
+
+        run();
+    }
+
+private:
+    wex::tcp &myTCPinput;
+    wex::timer *myStartTimer;
+    cIn2Out in2out;
+
+    void connect();
+    void input();
 };
 
-cIn2Out::cIn2Out()
-    : myInputPort(-1), myOutputPort(-1)
+cIn2Out::cIn2Out() : myInputPort(-1), myOutputPort(-1)
+
 {
 }
 
@@ -69,9 +123,54 @@ void cIn2Out::ParseOptions(int ac, char **av)
     }
 }
 
+void cGUI::connect()
+{
+    try
+    {
+        myTCPinput.server(in2out.sInputPort());
+        std::cout << "Waiting for input on port "
+                  << in2out.inputPort() << "\n";
+    }
+    catch (std::runtime_error &e)
+    {
+        std::cout << "Cannot start server " << e.what() << "\n";
+    }
+
+    // else
+    // {
+    //     try
+    //     {
+    //         myTCP.client();
+    //     }
+    //     catch (std::runtime_error &e)
+    //     {
+    //         status(std::string("Cannot connect to server ") + e.what());
+    //     }
+    // }
+}
+
+void cGUI::input()
+{
+    if (!myTCPinput.isConnected())
+    {
+        std::cout << "Input Connection closed, waiting for new client\n";
+
+        myTCPinput.server(in2out.sInputPort());
+        return;
+    }
+    auto msg = myTCPinput.readMsg();
+
+    // ascii
+    std::cout << "Input: " + myTCPinput.readMsg() << "\n\n";
+
+    // setup for next message
+    myTCPinput.read();
+}
+
 main(int argc, char *argv[])
 {
-    cIn2Out in2out;
-    in2out.ParseOptions(argc, argv);
+
+    cGUI theGUI(argc, argv);
+
     return 0;
 }
