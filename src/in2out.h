83a929818e47abc0856d24069b73d7d963d274c9
JamesBremner <james@ravenspoint.com>
2022-08-24 09:09:56 -0400

    Privat method docs

--- src/in2out.h
+++ src/in2out.h
@@ -33,9 +33,6 @@ public:
      *
      * Call this once when everything has been setup
      *
-     * This is used by console type applications.
-     * GUI applications should not call this
-     * They will call run on the main application window when setup is complete
      */
     void run();
 
@@ -57,12 +54,49 @@ private:
 
     std::thread *myKeyboardThread;
 
+    // run some unit tests
     void test();
+
+    /** Monitor input on the keyboard
+     * 
+     * Blocks - so run this in its own thread
+     * If "exit<enter>" is typed this will exit the app.
+     */
     void keyboardmonitor();
+
+    /** Attempt connection to output server - the listener
+     * 
+     * Asynchronous - this returns immediatly.
+     * At some later moment the connection may or may not be made
+     * The connection status will be discovered 
+     * when the next write output is run - don't do so immediatly!
+     */
     void connectOutputServer();
+
+    /** Handle loss of server connection
+     * 
+     * - display message
+     * - discard any data waiting to be output
+     * - start new connection attempt
+     */
     void noServerHandler();
+
+    /// Display input message as text and as hexadecimal bytes
     void inputDisplay(const std::string &msg);
+
+    // Count line waiting to be output, not including partial
     int countLines() const;
+
+    /** Check for last data line in a package
+     * @param[in] line1 the line to test
+     * @param[in] line2 the line that will be output after line1
+     * @return true if line1 is the last data line of a package
+     * 
+     * line1 is the last data line in a package
+     * if it is a data line and line is not.
+     * 
+     * Details: https://github.com/JamesBremner/in2out/wiki/Input
+     */
     bool isLastData(
         const std::string &line1,
         const std::string &line2) const;
